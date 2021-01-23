7ac8e0565a110291cff0d6a76f36ae509916ab62
Ken Ho <kenho12251999@gmail.com>
2021-01-22 16:50:36 -0800

    organized file structure and was able to render Bar svg component instead of using background colors to draw it

---
+++ src/components/Bar/Bar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {ReactComponent as BarImg} from './bar.svg';
+
+class Bar extends React.Component {
+    render() {
+        return (<BarImg />);
+    }
+}
+
+export default Bar;
---
+++ src/components/BarManager/BarManager.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import generateBar from './GenerateBar.js';
+
+class BarManager extends React.Component {
+    render() {
+        return (    
+            generateBar()
+        );
+    }
+}
+
+export default BarManager;
